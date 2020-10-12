3ad5f07ebbd66372a38114da0d94c132c872f5c5
Mike Cotterman <mcotterman@users.noreply.github.com>
2020-10-12 01:19:20 -0400

    Working on mouth

--- main.ts
+++ main.ts
@@ -2,15 +2,16 @@ function displayScreen (text: string) {
     basic.showString(text)
 }
 input.onButtonPressed(Button.A, function () {
-    if (blinkOn == 0) {
-        blinkOn = 1
-    } else {
-        blinkOn = 0
-    }
+    let smi: number[] = []
+    max7219_matrix.displayCustomCharacter(
+    smi,
+    0,
+    true
+    )
 })
 function blink () {
     while (true) {
-        if (blinkOn == 0) {
+        if (blinkOn == 1) {
             if (eyeStatus == 1) {
                 controlEyes("close")
                 eyeStatus = 0
@@ -25,22 +26,16 @@ function blink () {
     }
 }
 function mouth (status: string) {
-    max7219_matrix.addCustomChr(
-    "B00100000,B01000000,B10000110,B10000000,B10000000,B10000110,B01000000,B00100000",
-    [0, 1]
-    )
-    max7219_matrix.displayCustomCharacter(
-    [0, 1],
+    showMouth = smile
+    switch(status) {
+        case 'frown':
+            showMouth = frown;
+            break;
+    }
+max7219_matrix.displayCustomCharacter(
+    showMouth,
     0,
     true
-    )
-    max7219_matrix.displayText(
-    "Hi!",
-    0,
-    true
-    )
-    smile = max7219_matrix.getCustomCharacterArray(
-    "B00100000,B01000000,B10000110,B10000000,B10000000,B10000110,B01000000,B00100000"
     )
 }
 function controlEyes (status: string) {
@@ -60,13 +55,21 @@ radio.onReceivedString(function (receivedString) {
     basic.pause(5000)
     pins.servoWritePin(AnalogPin.P16, 90)
 })
+input.onButtonPressed(Button.B, function () {
+    if (blinkOn == 1) {
+        blinkOn = 0
+    } else {
+        blinkOn = 1
+    }
+})
+let showMouth: number[] = []
 let blinkTimer = 0
 let lastRadio = 0
+let smile: number[] = []
 let eyeStatus = 0
 let eyesClosed = 0
 let eyesOpen = 0
 let blinkOn = 0
-let smile: number[] = []
 blinkOn = 0
 eyesOpen = 87
 eyesClosed = 160
@@ -78,6 +81,24 @@ DigitalPin.P16,
 DigitalPin.P15,
 DigitalPin.P14,
 DigitalPin.P13
+)
+smile = max7219_matrix.getCustomCharacterArray(
+"B00000000,B00000000,B00000000,B11000011,B11000011,B11000011,B11111111,B01111110"
+)
+let kiss = max7219_matrix.getCustomCharacterArray(
+"B01111110,B01111110,B11000011,B11000011,B11000011,B11000011,B11111111,B01111110"
+)
+let frown = max7219_matrix.getCustomCharacterArray(
+"B00000000,B00000000,B00000000,B01111110,B11111111,B11000011,B11000011,B11000011"
+)
+let denied = max7219_matrix.getCustomCharacterArray(
+"B11000011,B11100111,B01111110,B00011000,B00011000,B01111110,B11100111,B11000011"
+)
+let meh = max7219_matrix.getCustomCharacterArray(
+"B00000000,B00000000,B00000000,B11111111,B11111111,B00000000,B00000000,B00000000"
+)
+let derp = max7219_matrix.getCustomCharacterArray(
+"B00000000,B00000000,B11000011,B11000011,B11111111,B01111110,B00001110,B00000100"
 )
 const mouthShapes = {
     smile: max7219_matrix.getCustomCharacterArray(
